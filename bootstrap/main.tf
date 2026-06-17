9405b96c1ca091b5bbc592184ee25f854aff9435
joao-henrike <jb.goncalves2406@gmail.com>
2026-06-16 20:48:52 -0400

    Pipeline estruturada: Gatekeeper com terraform plan para validacao total antes do deploy

--- bootstrap/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" { 
-  region = "us-east-1" 
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "techstock-tfstate-joao-7733"
-  tags   = { Name = "bucket-estado-terraform" }
-}
