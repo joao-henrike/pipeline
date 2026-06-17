4da35394e390a3bede54002831581ef8345729f6
joao-henrike <jb.goncalves2406@gmail.com>
2026-06-16 20:35:33 -0400

    Ajustes de rede e perfil de instancia para o ec2

--- aws/compute.tf
+++ aws/compute.tf
@@ -8,11 +8,15 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "backend_api" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public_ec2.id
-  security_groups = [aws_security_group.sg_backend.id]
-  key_name        = aws_key_pair.generated_key.key_name
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_ec2.id
+  vpc_security_group_ids      = [aws_security_group.sg_backend.id]
+  key_name                    = aws_key_pair.generated_key.key_name
   
+  # Parâmetros obrigatórios para o ambiente AWS Academy
+  iam_instance_profile        = "LabInstanceProfile"
+  associate_public_ip_address = true
+
   tags = { Name = "vm-backend-techstock" }
 }
--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,3 +1,11 @@
 output "ec2_public_ip" { value = aws_instance.backend_api.public_ip }
 output "rds_endpoint" { value = aws_db_instance.estoque_db.endpoint }
 output "s3_website_url" { value = aws_s3_bucket_website_configuration.frontend_web.website_endpoint }
+
+# ==========================================
+# CHAVE SSH PARA ACESSO (NOVO)
+# ==========================================
+output "chave_ssh_privada" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
+}
