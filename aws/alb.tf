8d62d0f51deb4503f5f11794c0b12b44395b7163
JOAO HENRIQUE PEREIRA GONCALVES <38773526851@info.local>
2026-06-17 16:55:28 -0300

    fix(infra): Restauracao do ALB e Target Groups para sustentar as exigencias dos scripts de startup

---
+++ aws/alb.tf
@@ -0,0 +1,103 @@
+# Subnet publica na Zona B obrigatoria para o ALB
+resource "aws_subnet" "public_ec2_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.10.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "subnet-public-b-techstock" }
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_ec2_b.id
+  route_table_id = aws_subnet.public_ec2.map_public_ip_on_launch ? aws_subnet.public_ec2.id : aws_subnet.public_ec2.id
+}
+
+# ==========================================
+# APPLICATION LOAD BALANCER
+# ==========================================
+resource "aws_lb" "main_alb" {
+  name               = "techstock-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_alb.id]
+  subnets            = [aws_subnet.public_ec2.id, aws_subnet.public_ec2_b.id]
+}
+
+# ==========================================
+# TARGET GROUPS (Destinos)
+# ==========================================
+resource "aws_lb_target_group" "frontend_tg" {
+  name     = "tg-frontend-techstock"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check { path = "/health" }
+}
+
+resource "aws_lb_target_group" "backend_tg" {
+  name     = "tg-backend-techstock"
+  port     = 3000 # O nodejs do seu script roda na 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check { path = "/api/health" }
+}
+
+resource "aws_lb_target_group" "monitoring_tg" {
+  name     = "tg-monitoring-techstock"
+  port     = 80 # O Nginx reverso do Grafana roda na 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check { path = "/grafana/api/health" }
+}
+
+# Fixando a maquina de monitoramento no Target Group dela
+resource "aws_lb_target_group_attachment" "monitoring_attach" {
+  target_group_arn = aws_lb_target_group.monitoring_tg.arn
+  target_id        = aws_instance.monitoramento.id
+  port             = 80
+}
+
+# ==========================================
+# REGRAS DO LISTENER (Atendendo ao seu Script)
+# ==========================================
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  # Fallback padrao vai para o Frontend
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "grafana_routing" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 10
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.monitoring_tg.arn
+  }
+  condition { path_pattern { values = ["/grafana*"] } }
+}
+
+resource "aws_lb_listener_rule" "prometheus_routing" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 20
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.monitoring_tg.arn
+  }
+  condition { path_pattern { values = ["/prometheus*"] } }
+}
+
+resource "aws_lb_listener_rule" "api_routing" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 30
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
+  condition { path_pattern { values = ["/api*"] } }
+}
--- aws/security.tf
+++ aws/security.tf
@@ -1,157 +1,51 @@
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = "chave-vms-techstock"
-  public_key = tls_private_key.ssh_key.public_key_openssh
-}
+resource "tls_private_key" "ssh_key" { algorithm = "RSA"; rsa_bits = 4096 }
+resource "aws_key_pair" "generated_key" { key_name = "chave-vms-techstock"; public_key = tls_private_key.ssh_key.public_key_openssh }
 
 resource "aws_security_group" "sg_eice" {
-  name        = "endpoint-conexao"
-  description = "Permite tunel SSH interno"
-  vpc_id      = aws_vpc.main.id
-  egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.0.0/16"]
-  }
+  name = "endpoint-conexao"
+  vpc_id = aws_vpc.main.id
+  egress { from_port = 22; to_port = 22; protocol = "tcp"; cidr_blocks = ["10.1.0.0/16"] }
 }
 
-# ==========================================
-# 1. FIREWALL DO FRONTEND (Vitrine)
-# ==========================================
+resource "aws_security_group" "sg_alb" {
+  name = "techstock-alb-sg"
+  vpc_id = aws_vpc.main.id
+  ingress { from_port = 80; to_port = 80; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
+  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+}
+
 resource "aws_security_group" "sg_frontend" {
-  name        = "frontend-ui"
-  description = "Acesso de usuarios ao sistema web"
-  vpc_id      = aws_vpc.main.id
-  
-  ingress {
-    description     = "SSH via Endpoint"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_eice.id]
-  }
-  
-  ingress {
-    description = "Trafego HTTP do mundo"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name = "frontend-ui"
+  vpc_id = aws_vpc.main.id
+  ingress { from_port = 22; to_port = 22; protocol = "tcp"; security_groups = [aws_security_group.sg_eice.id] }
+  ingress { from_port = 80; to_port = 80; protocol = "tcp"; security_groups = [aws_security_group.sg_alb.id] }
+  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
 }
 
-# ==========================================
-# 2. FIREWALL DO BACKEND (Trancado)
-# ==========================================
 resource "aws_security_group" "sg_backend" {
-  name        = "backend-api"
-  description = "Regras restritas da API"
-  vpc_id      = aws_vpc.main.id
-  
-  ingress {
-    description     = "SSH via Endpoint"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_eice.id]
-  }
-  
-  ingress {
-    description     = "Trafego HTTP APENAS do Frontend"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_frontend.id] # Elo de confianca
-  }
-
-  ingress {
-    description = "Permite coleta de metricas do Monitoramento"
-    from_port   = 9100 # Porta padrao do Node Exporter
-    to_port     = 9100
-    protocol    = "tcp"
-    cidr_blocks = ["10.1.0.0/16"] # Confia na rede interna (VPC)
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name = "backend-api"
+  vpc_id = aws_vpc.main.id
+  ingress { from_port = 22; to_port = 22; protocol = "tcp"; security_groups = [aws_security_group.sg_eice.id] }
+  ingress { from_port = 3000; to_port = 3000; protocol = "tcp"; security_groups = [aws_security_group.sg_alb.id, aws_security_group.sg_frontend.id] }
+  ingress { from_port = 9100; to_port = 9100; protocol = "tcp"; cidr_blocks = ["10.1.0.0/16"] }
+  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
 }
 
-# ==========================================
-# 3. FIREWALL DO MONITORAMENTO (Telemetria)
-# ==========================================
 resource "aws_security_group" "sg_monitoramento" {
-  name        = "telemetria-grafana"
-  description = "Painel de operacoes SecOps"
-  vpc_id      = aws_vpc.main.id
-  
-  ingress {
-    description     = "SSH via Endpoint"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_eice.id]
-  }
-  
-  ingress {
-    description = "Painel Grafana web"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Painel Prometheus web"
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name = "telemetria-grafana"
+  vpc_id = aws_vpc.main.id
+  ingress { from_port = 22; to_port = 22; protocol = "tcp"; security_groups = [aws_security_group.sg_eice.id] }
+  ingress { from_port = 80; to_port = 80; protocol = "tcp"; security_groups = [aws_security_group.sg_alb.id] }
+  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
 }
 
-# ==========================================
-# 4. FIREWALL DO RDS (Cofre)
-# ==========================================
 resource "aws_security_group" "sg_database" {
-  name        = "rds-estoque"
-  description = "Bloqueia tudo, exceto a API"
-  vpc_id      = aws_vpc.main.id
-  
-  ingress {
-    description     = "Trafego Postgres vindo APENAS do Backend"
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_backend.id]
-  }
+  name = "rds-estoque"
+  vpc_id = aws_vpc.main.id
+  ingress { from_port = 5432; to_port = 5432; protocol = "tcp"; security_groups = [aws_security_group.sg_backend.id] }
 }
 
 resource "aws_ec2_instance_connect_endpoint" "eice" {
-  subnet_id          = aws_subnet.public_ec2.id
+  subnet_id = aws_subnet.public_ec2.id
   security_group_ids = [aws_security_group.sg_eice.id]
-  preserve_client_ip = false
-  tags = { Name = "eice-techstock" }
 }
