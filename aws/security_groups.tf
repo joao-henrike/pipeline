625fdb92183c579e6b49aca259bf69a8dfa3871a
joao-henrike <jb.goncalves2406@gmail.com>
2026-06-19 15:25:38 -0400

    fix(firewall): expansao dos blocos hcl para compatibilidade estrita do compilador

---
+++ aws/security_groups.tf
@@ -0,0 +1,70 @@
+resource "aws_security_group" "sg_alb" {
+  name        = "sg_alb"
+  description = "Permite trafego web para o Load Balancer"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_monitoramento" {
+  name        = "sg_monitoramento"
+  description = "Permite SSH e Web para a maquina de telemetria"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_database" {
+  name        = "sg_database"
+  description = "Permite trafego interno para o Banco de Dados"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
