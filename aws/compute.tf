c644772d4cf3b4c4a3280d6ba7e6a047c596fdc7
joao-henrike <jb.goncalves2406@gmail.com>
2026-06-21 21:38:32 -0300

    fix(network): exposicao publica temporaria do monitoramento para validacao

--- aws/compute.tf
+++ aws/compute.tf
@@ -14,7 +14,6 @@ resource "aws_instance" "ec2_frontend" {
   vpc_security_group_ids = [aws_security_group.sg_frontend.id]
   key_name               = aws_key_pair.generated_key.key_name
   iam_instance_profile   = "LabInstanceProfile"
-
   user_data = templatefile("${path.module}/scripts/frontend.tftpl", {
     alb_dns = aws_lb.main_alb.dns_name
   })
@@ -28,7 +27,6 @@ resource "aws_instance" "ec2_backend" {
   vpc_security_group_ids = [aws_security_group.sg_backend.id]
   key_name               = aws_key_pair.generated_key.key_name
   iam_instance_profile   = "LabInstanceProfile"
-
   user_data = templatefile("${path.module}/scripts/backend.tftpl", {
     db_host     = aws_db_instance.postgres.address
     db_name     = aws_db_instance.postgres.db_name
@@ -40,20 +38,19 @@ resource "aws_instance" "ec2_backend" {
 }
 
 resource "aws_instance" "ec2_monitoring" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_subnet.id
-  vpc_security_group_ids = [aws_security_group.sg_frontend.id]
-  key_name               = aws_key_pair.generated_key.key_name
-  iam_instance_profile   = "LabInstanceProfile"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg_frontend.id]
+  key_name                    = aws_key_pair.generated_key.key_name
+  iam_instance_profile        = "LabInstanceProfile"
 
   user_data = templatefile("${path.module}/scripts/monitoring.tftpl", {
     alb_dns          = aws_lb.main_alb.dns_name
     backend_ip       = aws_instance.ec2_backend.private_ip
     grafana_password = "TechStock@2026!"
-
-    # Satisfazendo as duas exigencias do compilador para liberar o destroy
-    github_raw_url = "https://raw.githubusercontent.com/SEU_USUARIO/pipeline/main/monitoring/grafana/dashboards"
+    github_raw_url   = "https://raw.githubusercontent.com/SEU_USUARIO/pipeline/main/monitoring/grafana/dashboards"
   })
   tags = { Name = "techstock-ec2-monitoring" }
 }
